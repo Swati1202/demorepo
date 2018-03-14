be6845bcef09edff4f4c1f8639d0a4a152669348
Swati <swati@mobisir.net>
2018-03-14 18:02:41 +0530

    demoooo class added

---
+++ src/test/java/com/demo1/page/demooooooooo.java
@@ -0,0 +1,18 @@
+package com.demo1.page;
+
+import org.testng.annotations.Test;
+
+public class demooooooooo {
+
+	
+	@Test
+	public void first()
+	{
+		System.out.println("sjdddddddddddddddg");
+	}
+	
+	
+	
+	
+	
+}
